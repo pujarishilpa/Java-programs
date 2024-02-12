5ba9fc6b219f49b15cfdcecafda366726ee1bffe
pujari shilpa <shilpapujari286@gmail.com>
2024-02-12 20:11:35 +0530

    Add Set Bit

---
+++ Loops.java
@@ -0,0 +1,16 @@
+class Loops {
+    public static void main(String args[]) {
+        for(int i = 1; i<=5; i++)
+            System.out.println(i);
+        int i = 6;
+        while(i<=10) {
+            System.out.println(i);
+            i++;
+        }
+        int a = 11;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a<=15);
+    }
+}
