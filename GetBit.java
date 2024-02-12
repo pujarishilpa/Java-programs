a31d6435d43a3d643676493c16b6066dbda2afc3
pujari shilpa <shilpapujari286@gmail.com>
2024-02-12 19:59:26 +0530

    Add Get Bit

---
+++ GetBit.java
@@ -0,0 +1,13 @@
+public class GetBit {
+    public static void main(String args[]) {
+        int n = 5;
+        int pos = 2;
+        int bitmask = 1 << pos;
+        if((bitmask &n) == 0) {
+            System.out.println("Bit was zero");
+        }
+        else {
+            System.out.println("Bit was one");
+        }
+    }
+}
